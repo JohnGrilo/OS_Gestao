963a0221ab731db47118e6934d704317d080f191
JohnGrilo <johngrilo@gmail.com>
2024-08-18 20:25:49 -0300

    Tela de Login e Modulo de Conexao BD

---
+++ Grilo_Company/src/br/com/grilocomp/dal/ModuloConexao.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.grilocomp.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author 55349
+ */
+public class ModuloConexao {
+
+    //Método por conectar com o BD
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        // Chama o driver de conexao - Biblioteca
+        String driver = "com.mysql.cj.jdbc.Driver";
+        // Armazenando info referente ao BD
+        String url = "jdbc:mysql://localhost:3306/dbgrilocomp";
+        String user = "root";
+        String password = "grilo1234";
+        // Conectando com o BD
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            //System.out.println(e);
+            return null;
+        }
+    }
+}
